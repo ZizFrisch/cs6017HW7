56e68d9594b99ea9bd4fff2e7609caa7e1f7b020
Elisabeth Frischknecht <elisabeth.frischknecht96@gmail.com>
2024-07-30 15:46:53 -0600

    3 divs, updated style sheet, bar chart now in the left

--- static/js/script.js
+++ static/js/script.js
@@ -3,24 +3,30 @@ import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 window.onload = async function(){
     let response = await fetch("/static/data/medals.csv")
     let medals = await response.text();
-    console.log("CSV data:", medals);
+    // console.log("CSV data:", medals);
     //let medals = await d3.text("/static/data/medals.csv"); //returns a "promise"
 
     //parse the csv data
     let parsedData = d3.csvParse(medals, d3.autoType);
-    console.log("Parsed data:", parsedData);
+    // console.log("Parsed data:", parsedData);
 
     //create a bar chart
     const margin = {top: 20, right:30, bottom: 40, left:90};
-    const width = 800 - margin.left - margin.right;
-    const height = 400 - margin.top - margin.bottom;
+    const div = document.getElementById("leftDiv");
+    const divWidth = div.clientWidth;
+    const divHeight = div.clientHeight;
+    const width = divWidth - margin.left - margin.right;
+    const height = divHeight - margin.top - margin.bottom;
 
-    const svg = d3.select("#chart")
+    //clear any existing content in the leftDiv
+    d3.select("#leftDiv").selectAll("*").remove();
+
+    const svg = d3.select("#leftDiv")
         .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", divWidth)
+        .attr("height", divHeight)
         .append("g")
-        .attr("transform", 'translate(${margin.left},${margin.top})');
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
     const x = d3.scaleLinear()
         .domain([0, d3.max(parsedData, d=> d.Total)])
@@ -46,50 +52,6 @@ window.onload = async function(){
         .call(d3.axisLeft(y));
 
     svg.append("g")
-        .attr("transform", 'translate(0,${height})')
+        .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x));
-
 };
-
-
-// d3.json('/data').then(function(data) {
-//     const svg = d3.select('#chart')
-//                   .append('svg')
-//                   .attr('width', 600)
-//                   .attr('height', 400);
-
-//     const margin = {top: 20, right: 30, bottom: 40, left: 40},
-//           width = +svg.attr('width') - margin.left - margin.right,
-//           height = +svg.attr('height') - margin.top - margin.bottom;
-
-//     const x = d3.scaleBand()
-//                 .domain(data.map(d => d.Category))
-//                 .range([margin.left, width - margin.right])
-//                 .padding(0.1);
-
-//     const y = d3.scaleLinear()
-//                 .domain([0, d3.max(data, d => d.Value)])
-//                 .nice()
-//                 .range([height - margin.bottom, margin.top]);
-
-//     const xAxis = g => g
-//                       .attr('transform', `translate(0,${height - margin.bottom})`)
-//                       .call(d3.axisBottom(x).tickSizeOuter(0));
-
-//     const yAxis = g => g
-//                       .attr('transform', `translate(${margin.left},0)`)
-//                       .call(d3.axisLeft(y))
-//                       .call(g => g.select('.domain').remove());
-
-//     svg.append('g')
-//        .selectAll('rect')
-//        .data(data)
-//        .join('rect')
-//        .attr('x', d => x(d.Category))
-//        .attr('y', d => y(d.Value))
-//        .attr('height', d => y(0) - y(d.Value))
-//        .attr('width', x.bandwidth());
-
-//     svg.append('g').call(xAxis);
-//     svg.append('g').call(yAxis);
-// });
